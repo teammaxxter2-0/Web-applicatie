eca00dc0a7b0e46de41600bd0cd6dd8f13e9e860
Luuk <luuk.volkering@gmail.com>
2024-05-23 11:38:30 +0200

    loguit knop

--- Front-end/OfferteApp/src/Components/Navbar.tsx
+++ Front-end/OfferteApp/src/Components/Navbar.tsx
@@ -1,7 +1,13 @@
-
 import { Link } from 'react-router-dom';
+import {Button} from "reactstrap";
+import {useState} from "react";
 
 function Navbar() {
+    const [loggedIn, setLoggedIn] = useState(localStorage.getItem('token') !== null);
+    const logout = () => {
+        setLoggedIn(false);
+        localStorage.clear();
+    }
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
@@ -21,6 +27,12 @@ function Navbar() {
                         <li className="nav-item">
                             <Link className="nav-link" to="/winkelwagen">Winkelwagen  </Link>
                         </li>
+
+                        {loggedIn &&
+                        <li className="nav-item">
+                          <Button className="nav-link" onClick={logout}>Logout</Button>
+                        </li>
+                        }
 
                     </ul>
                 </div>
