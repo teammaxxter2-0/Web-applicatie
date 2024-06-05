37a3f18d127ee9e6d91b017a3a4cce284dcbc772
Giovanni <124798862+1058989@users.noreply.github.com>
2024-06-05 16:52:55 +0200

    admin offerte + home pagina (#6)
    admin offerte geneeer pdf knop en accepteer mooier gemaakt. ook is er nu een home pagina met wat info

--- Front-end/OfferteApp/src/Pages/App.tsx
+++ Front-end/OfferteApp/src/Pages/App.tsx
@@ -1,19 +1,53 @@
 import '../Styles/App.css';
-import Navbar from '../Components/Navbar.tsx';
+import Navbar from '../Components/Navbar';
+import { useNavigate } from 'react-router-dom';
+import { Card, Button } from 'antd';
 
 function App() {
-    
+    const navigate = useNavigate();
     return (
         <>
-           <Navbar />
-            <h1>Blis Digital</h1>
+            <Navbar />
+            <div className="d-flex justify-content-center mt-5">
+                <Card
+                    title="Blis Digital"
+                    bordered={true}
+                    className="w-75"
+                    style={{ maxWidth: '1000px' }}
+                >
+                    <h3> Welkom bij Blis Digital</h3>
 
-            <li>
-                <a href={`/chatbot`}>Chat bot</a>
-            </li>
-            <li>
-                <a href={`/catalog`}>Catalogus</a>
-            </li>
+                    <p>
+                        Bij Blis Digital kunt u op twee verschillende manieren spullen bestellen: 
+                    </p>
+                    <p>
+                        <strong>1. Via de Catalogus:</strong> Blader door onze uitgebreide catalogus om het materiaal van uw keuze te vinden en geef vervolgens de gewenste specificaties op.
+                    </p>
+                    <p>
+                        <strong>2. Via de Chatbot:</strong> Praat met onze AI-chatbot om u te helpen bij het kiezen van de juiste producten en het plaatsen van uw bestelling.
+                    </p>
+                    <p>
+                        Bovendien kunt u uw gekozen spullen bekijken in uw winkelwagen voordat u de bestelling plaatst. 
+                    </p>
+                    <p>
+                        Beheerders kunnen gebruik maken van de beheerderfunctionaliteiten voor extra opties.
+                    </p>
+                    <div className="d-flex justify-content-between mb-5">
+                        <Button style={{ width: '200px' }} onClick={() => navigate('/catalog')}>
+                            Catalogus
+                        </Button>
+                        <Button style={{ width: '200px' }} onClick={() => navigate('/chatbot')}>
+                            Chatbot
+                        </Button>
+                        <Button style={{ width: '200px' }} onClick={() => navigate('/winkelwagen')}>
+                            Winkelwagen
+                        </Button>
+                        <Button style={{ width: '200px' }} onClick={() => navigate('/beheerder')}>
+                            Beheer
+                        </Button>
+                    </div>
+                </Card>
+            </div>
         </>
     );
 }
--- Front-end/OfferteApp/src/Pages/admin-see-offertes.tsx
+++ Front-end/OfferteApp/src/Pages/admin-see-offertes.tsx
@@ -1,13 +1,13 @@
 import { useRef, useState, useEffect } from 'react';
-import { SearchOutlined, FormOutlined, LoadingOutlined } from '@ant-design/icons';
-import {Button, Input, Space, Table, Modal, message} from 'antd';
+import { SearchOutlined, FormOutlined, LoadingOutlined, InfoCircleOutlined, FilePdfOutlined, CheckOutlined } from '@ant-design/icons';
+import { Button, Input, Space, Table, Modal, message, Flex } from 'antd';
 import type { InputRef, TableColumnsType, TableColumnType } from 'antd';
 import type { FilterDropdownProps } from 'antd/es/table/interface';
 import Navbar from '../Components/Navbar.tsx';
 import { useNavigate } from 'react-router-dom';
 import useToken from "../hooks/Token.ts";
 import GeneratePDF from "../Components/GeneratePDF.ts";
-import {Offerte} from "../interfaces/Offerte.ts";
+import { Offerte } from "../interfaces/Offerte.ts";
 
 type DataIndex = keyof Offerte;
 
@@ -52,9 +52,9 @@ function AdminSeeOffertes() {
   };
 
   const handleSearch = (
-      selectedKeys: string[],
-      confirm: FilterDropdownProps['confirm'],
-      dataIndex: DataIndex,
+    selectedKeys: string[],
+    confirm: FilterDropdownProps['confirm'],
+    dataIndex: DataIndex,
   ) => {
     confirm();
     setSearchText(selectedKeys[0]);
@@ -67,7 +67,7 @@ function AdminSeeOffertes() {
   };
 
   const accepteer = async (id: number) => {
-    try{
+    try {
       const response = await fetch(`/api/Quotation/${id}/accept`, {
         method: "GET",
         headers: {
@@ -75,12 +75,13 @@ function AdminSeeOffertes() {
           authorization: `Bearer ${token}`
         }
       })
-    if (response.ok) {
-      message.success("De offerte is geaccepteerd!")
-    }else{
-      message.error("Het is niet gelukt om de offerte te accepteren.")
-    }
-    }catch(err){
+      if (response.ok) {
+        message.success("De offerte is geaccepteerd!");
+        setSelectedOption((prev) => ({ ...prev, accepted: true }));
+      } else {
+        message.error("Het is niet gelukt om de offerte te accepteren.")
+      }
+    } catch (err) {
       console.error(err);
       message.error("Het is niet gelukt om de offerte te accepteren.")
     }
@@ -89,43 +90,43 @@ function AdminSeeOffertes() {
 
   const getColumnSearchProps = (dataIndex: DataIndex): TableColumnType<Offerte> => ({
     filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
-        <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
-          <Input
-              ref={searchInput}
-              placeholder={`Zoek naar ${dataIndex}`}
-              value={selectedKeys[0]}
-              onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-              onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-              style={{ marginBottom: 8, display: 'block' }}
-          />
-          <Space>
-            <Button
-                type="primary"
-                onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-                icon={<SearchOutlined />}
-                size="small"
-                style={{ width: 90 }}
-            >
-              Zoek
-            </Button>
-            <Button
-                onClick={() => clearFilters && handleReset(clearFilters)}
-                size="small"
-                style={{ width: 90 }}
-            >
-              Reset
-            </Button>
-          </Space>
-        </div>
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Zoek naar ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Zoek
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+        </Space>
+      </div>
     ),
     filterIcon: () => (
-        <SearchOutlined />
+      <SearchOutlined />
     ),
     onFilter: (value, record) =>
-        record[dataIndex]
-            .toString()
-            .toLowerCase()
-            .includes((value as string).toLowerCase()),
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
     onFilterDropdownOpenChange: (visible) => {
       if (visible) {
         setTimeout(() => searchInput.current?.select(), 100);
@@ -147,10 +148,19 @@ function AdminSeeOffertes() {
 
   const columns: TableColumnsType<Offerte> = [
     {
+      title: 'id',
+      dataIndex: 'id',
+      key: 'id',
+      width: '20%',
+      ...getColumnSearchProps('id'),
+      sorter: (a, b) => a.id - b.id,
+      render: (id: number) => `${id}`,
+    },
+    {
       title: 'name',
       dataIndex: 'name',
       key: 'name',
-      width: '20%',
+      width: '25%',
       ...getColumnSearchProps('name'),
     },
     {
@@ -159,103 +169,130 @@ function AdminSeeOffertes() {
       key: 'offerte_prijs_totaal',
       width: '20%',
       ...getColumnSearchProps('offerte_prijs_totaal'),
+      sorter: (a, b) => a.offerte_prijs_totaal - b.offerte_prijs_totaal,
+      render: (price: number) => `€ ${price}`,
     },
     {
       title: 'Geaccepteerd',
       dataIndex: 'accepted',
       key: 'accepted',
-      width: '20%',
+      width: '25%',
       render: (_text, record) => (record.accepted ? 'Ja' : 'Nee'),
       filters: [
         { text: 'Ja', value: true },
         { text: 'Nee', value: false },
       ],
       onFilter: (value, record) => record.accepted === value,
-      ...getColumnSearchProps('accepted'),
     },
     {
       title: 'Acties',
       key: 'actions',
       render: (record) => (
-          <Space size="middle">
-            <Button type="link" onClick={() => handleDetails(record)}> <FormOutlined /></Button>
-          </Space>
+        <Space size="middle">
+          <Button type="link" onClick={() => handleDetails(record)}> <InfoCircleOutlined /></Button>
+        </Space>
       ),
     },
   ];
 
   return (
-      <>
-        <Navbar />
-        <div className="container mt-3">
-          {loading && <h1>Loading... <LoadingOutlined /></h1>}
-          {error && <p>{error}</p>}
-          {!loading && !error && (
-              <>
-                <Table columns={columns} dataSource={bestellingen} pagination={{ pageSize: 7 }} />
-              </>
-          )}
-          <Button type="primary" style={{ marginLeft: 55 }} onClick={() => navigate(-1)}>
-            Ga terug
-          </Button>
+    <>
+      <Navbar />
+      <div className="container mt-3">
+        {loading && <h1>Loading... <LoadingOutlined /></h1>}
+        {error && <p>{error}</p>}
+        {!loading && !error && (
+          <>
+            <Table columns={columns} dataSource={bestellingen} pagination={{ pageSize: 7 }} />
+          </>
+        )}
+        <Button type="primary" style={{ marginLeft: 55 }} onClick={() => navigate(-1)}>
+          Ga terug
+        </Button>
+      </div>
+
+      <Modal
+        title="Bestelling Info"
+        open={isViewModalVisible}
+        onCancel={handleViewModalClose}
+        width={600}
+        footer={[
+          <Button key="close" onClick={handleViewModalClose}>
+            Terug
+          </Button>,
+        ]}
+      >
+        <div>
+          <p><strong>Naam:</strong> {selectedOption.name}</p>
+          <p><strong>Aangemaakt op:</strong> {selectedOption.creation === undefined ? "Geen tijd bekend" : selectedOption.creation.toLocaleString()}</p>
+          <p><strong>Geaccepteerd:</strong> {selectedOption.accepted ? "Ja" : "Nee"}</p>
+          <p><strong>Aantal m2:</strong> {selectedOption.aantal_m2}</p>
+          <p><strong>Prijs per m2:</strong> €{selectedOption.prijs_per_m2}</p>
+          <p><strong>Prijs m2 Totaal:</strong> €{selectedOption.prijs_m2_totaal}</p>
+          <p><strong>Randafwerking:</strong> {selectedOption.randafwerking ? 'Ja' : 'Nee'}</p>
+          <p><strong>Randafwerking m:</strong> {selectedOption.randafwerking_m}</p>
+          <p><strong>Randafwerking Prijs per m:</strong> €{selectedOption.randafwerking_prijs_per_m}</p>
+          <p><strong>Randafwerking Hoogte (mm):</strong> {selectedOption.randafwerking_hoogte_mm}</p>
+          <p><strong>Randafwerking Prijs Totaal:</strong> €{selectedOption.randafwerking_prijs_totaal}</p>
+          <p><strong>Spatrand m:</strong> {selectedOption.spatrand_m}</p>
+          <p><strong>Spatrand Prijs per m:</strong> €{selectedOption.spatrand_prijs_per_m}</p>
+          <p><strong>Spatrand Hoogte (mm):</strong> {selectedOption.spatrand_hoogte_mm}</p>
+          <p><strong>Spatrand Prijs Totaal:</strong> €{selectedOption.spatrand_prijs_totaal}</p>
+          <p><strong>Vensterbank m:</strong> {selectedOption.vensterbank_m}</p>
+          <p><strong>Vensterbank Prijs per m:</strong> €{selectedOption.vensterbank_prijs_per_m}</p>
+          <p><strong>Vensterbank Breedte (mm):</strong> {selectedOption.vensterbank_breedte_mm}</p>
+          <p><strong>Vensterbank Prijs Totaal:</strong> €{selectedOption.vensterbank_prijs_totaal}</p>
+          <p><strong>Spoelbak:</strong> {selectedOption.spoelbak ? 'Ja' : 'Nee'}</p>
+          <p><strong>Uitsparing Spoelbak:</strong> {selectedOption.uitsparing_spoelbak}</p>
+          <p><strong>Spoelbak Prijs:</strong> €{selectedOption.spoelbak_prijs}</p>
+          <p><strong>Kraangat:</strong> {selectedOption.kraangat ? 'Ja' : 'Nee'}</p>
+          <p><strong>Kraangat Prijs:</strong> €{selectedOption.kraangat_prijs}</p>
+          <p><strong>Zeepdispenser:</strong> {selectedOption.zeepdispenser ? 'Ja' : 'Nee'}</p>
+          <p><strong>Zeepdispenser Prijs:</strong> €{selectedOption.zeepdispenser_prijs}</p>
+          <p><strong>Boorgaten:</strong> {selectedOption.boorgaten ? 'Ja' : 'Nee'}</p>
+          <p><strong>Boorgaten Stuk:</strong> {selectedOption.boorgaten_stuk}</p>
+          <p><strong>Boorgaten mm:</strong> {selectedOption.boorgaten_mm}</p>
+          <p><strong>Boorgaten Prijs per Stuk:</strong> €{selectedOption.boorgaten_prijs_per_stuk}</p>
+          <p><strong>Boorgaten Prijs Totaal:</strong> €{selectedOption.boorgaten_prijs_totaal}</p>
+          <p><strong>WCD:</strong> {selectedOption.wcd ? 'Ja' : 'Nee'}</p>
+          <p><strong>WCD Prijs:</strong> €{selectedOption.wcd_prijs}</p>
+          <p><strong>Achterwand:</strong> {selectedOption.achterwand ? 'Ja' : 'Nee'}</p>
+          <p><strong>Achterwand m2:</strong> {selectedOption.achterwand_m2}</p>
+          <p><strong>Achterwand Prijs per m2:</strong> €{selectedOption.achterwand_prijs_per_m2}</p>
+          <p><strong>Achterwand Prijs Totaal:</strong> €{selectedOption.achterwand_prijs_totaal}</p>
+          <p><strong>Offerte Prijs Totaal:</strong> €{selectedOption.offerte_prijs_totaal}</p>
+          <Flex gap="middle" justify='center' >
+            <Button
+              type="primary"
+              style={{ width: '200px' }}
+              icon={<FilePdfOutlined />}
+              size="large"
+              onClick={() => GeneratePDF(selectedOption)}
+            >
+              Genereer PDF
+            </Button>
+            
+            </Flex>
+            <p></p>
+            <Flex gap="middle" justify='center' >
+            {!selectedOption.accepted && (
+              <Button
+                type="primary"
+                icon={<CheckOutlined />}
+                size="large"
+                style={{ width: '200px' }}
+                onClick={() => accepteer(selectedOption.id)}
+              >
+                Accepteer Offerte
+              </Button>
+
+            )}
+
+          </Flex>
         </div>
 
-
-        <Modal
-            title="Bestelling Info"
-            open={isViewModalVisible}
-            onCancel={handleViewModalClose}
-            width={600}
-            footer={[
-              <Button key="close" onClick={handleViewModalClose}>
-                Terug
-              </Button>,
-            ]}
-        >
-          <div>
-            <p><strong>Naam:</strong> {selectedOption.name}</p>
-            <p><strong>Aangemaakt op:</strong> {selectedOption.creation === undefined ? "Geen tijd bekend" : selectedOption.creation.toLocaleString()}</p>
-            <p><strong>Geaccepteerd:</strong> {selectedOption.accepted ? "Ja" : "Nee"}</p>
-            <p><strong>Aantal m2:</strong> {selectedOption.aantal_m2}</p>
-            <p><strong>Prijs per m2:</strong> €{selectedOption.prijs_per_m2}</p>
-            <p><strong>Prijs m2 Totaal:</strong> €{selectedOption.prijs_m2_totaal}</p>
-            <p><strong>Randafwerking:</strong> {selectedOption.randafwerking ? 'Ja' : 'Nee'}</p>
-            <p><strong>Randafwerking m:</strong> {selectedOption.randafwerking_m}</p>
-            <p><strong>Randafwerking Prijs per m:</strong> €{selectedOption.randafwerking_prijs_per_m}</p>
-            <p><strong>Randafwerking Hoogte (mm):</strong> {selectedOption.randafwerking_hoogte_mm}</p>
-            <p><strong>Randafwerking Prijs Totaal:</strong> €{selectedOption.randafwerking_prijs_totaal}</p>
-            <p><strong>Spatrand m:</strong> {selectedOption.spatrand_m}</p>
-            <p><strong>Spatrand Prijs per m:</strong> €{selectedOption.spatrand_prijs_per_m}</p>
-            <p><strong>Spatrand Hoogte (mm):</strong> {selectedOption.spatrand_hoogte_mm}</p>
-            <p><strong>Spatrand Prijs Totaal:</strong> €{selectedOption.spatrand_prijs_totaal}</p>
-            <p><strong>Vensterbank m:</strong> {selectedOption.vensterbank_m}</p>
-            <p><strong>Vensterbank Prijs per m:</strong> €{selectedOption.vensterbank_prijs_per_m}</p>
-            <p><strong>Vensterbank Breedte (mm):</strong> {selectedOption.vensterbank_breedte_mm}</p>
-            <p><strong>Vensterbank Prijs Totaal:</strong> €{selectedOption.vensterbank_prijs_totaal}</p>
-            <p><strong>Spoelbak:</strong> {selectedOption.spoelbak ? 'Ja' : 'Nee'}</p>
-            <p><strong>Uitsparing Spoelbak:</strong> {selectedOption.uitsparing_spoelbak}</p>
-            <p><strong>Spoelbak Prijs:</strong> €{selectedOption.spoelbak_prijs}</p>
-            <p><strong>Kraangat:</strong> {selectedOption.kraangat ? 'Ja' : 'Nee'}</p>
-            <p><strong>Kraangat Prijs:</strong> €{selectedOption.kraangat_prijs}</p>
-            <p><strong>Zeepdispenser:</strong> {selectedOption.zeepdispenser ? 'Ja' : 'Nee'}</p>
-            <p><strong>Zeepdispenser Prijs:</strong> €{selectedOption.zeepdispenser_prijs}</p>
-            <p><strong>Boorgaten:</strong> {selectedOption.boorgaten ? 'Ja' : 'Nee'}</p>
-            <p><strong>Boorgaten Stuk:</strong> {selectedOption.boorgaten_stuk}</p>
-            <p><strong>Boorgaten mm:</strong> {selectedOption.boorgaten_mm}</p>
-            <p><strong>Boorgaten Prijs per Stuk:</strong> €{selectedOption.boorgaten_prijs_per_stuk}</p>
-            <p><strong>Boorgaten Prijs Totaal:</strong> €{selectedOption.boorgaten_prijs_totaal}</p>
-            <p><strong>WCD:</strong> {selectedOption.wcd ? 'Ja' : 'Nee'}</p>
-            <p><strong>WCD Prijs:</strong> €{selectedOption.wcd_prijs}</p>
-            <p><strong>Achterwand:</strong> {selectedOption.achterwand ? 'Ja' : 'Nee'}</p>
-            <p><strong>Achterwand m2:</strong> {selectedOption.achterwand_m2}</p>
-            <p><strong>Achterwand Prijs per m2:</strong> €{selectedOption.achterwand_prijs_per_m2}</p>
-            <p><strong>Achterwand Prijs Totaal:</strong> €{selectedOption.achterwand_prijs_totaal}</p>
-            <p><strong>Offerte Prijs Totaal:</strong> €{selectedOption.offerte_prijs_totaal}</p>
-            <Button type="link" onClick={() => GeneratePDF(selectedOption)}>GENEREER PDF</Button>
-            <Button type={"link"} onClick={() => accepteer(selectedOption.id)}>Accepteer offerte</Button>
-          </div>
-        </Modal>
-      </>
+      </Modal>
+    </>
   );
 }
 
